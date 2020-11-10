71c3107581b09ce9bc0b71266c4c06f1ac31788c
César Jaldin <rodia.piedra@gmail.com>
2020-11-10 19:40:52 -0400

    Update the global config for heroku ecosystem

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,11 +1,45 @@
 <?php
+
+$db = parse_url(getenv("DATABASE_URL"));
+$dns = "pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/"));
+
 return [
     'api-tools-content-negotiation' => [
         'selectors' => [],
     ],
+    'api-tools-mvc-auth' => [
+        'authentication' => [
+            'adapters' => [
+                'oauth' => [
+                    'adapter' => \Laminas\ApiTools\MvcAuth\Authentication\OAuth2Adapter::class,
+                    'storage' => [
+                        'adapter' => \pdo::class,
+                        'dsn' => $dns,
+                        'route' => '/oauth',
+                        'username' => $db['user'],
+                        'password' => $db['pass'],
+                    ],
+                ],
+            ],
+        ],
+    ],
     'db' => [
         'adapters' => [
-            'PGAdapter' => [],
+            'PGAdapter' => [
+                'database' => ltrim($db["path"], "/"),
+                'driver' => 'PDO_Pgsql',
+                'hostname' => $db["host"],
+                'username' => $db["user"],
+                'password' => $db["pass"],
+                'port' => $db["port"],
+                'dsn' => $dsn,
+            ],
         ],
     ],
     'router' => [
@@ -18,5 +52,11 @@ return [
                 'type' => 'regex',
             ],
         ],
+    ],
+    'api-tools-oauth2' => [
+        'allow_implicit' => true,
+    ],
+    'EnterpriseLib' => [
+        'array_mapper_path' => 'data/enterpriselib.php',
     ],
 ];
